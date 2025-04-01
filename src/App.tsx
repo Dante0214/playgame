8ee14dc08f89a9aae859c5a36a59869d99da0392
Dante0214 <rn_chic@naver.com>
2025-04-01 18:07:27 +0900

    feat: 레이아웃 변경

--- src/App.tsx
+++ src/App.tsx
@@ -41,45 +41,47 @@ function App() {
   };
 
   return (
-    <div className=" flex flex-col items-center justify-center p-6 bg-gray-100 rounded-lg shadow-lg max-w-md mx-auto">
-      <h1>가위바위보 게임</h1>
-      <div className="flex justify-between w-full mb-8">
-        <Player
-          label="당신"
-          choice={userChoice}
-          score={score.user}
-          scoreColor="text-blue-600"
-        />
-        <ResultDisplay result={result} />
-        <Player
-          label="컴퓨터"
-          choice={computerChice}
-          score={score.computer}
-          scoreColor="text-red-600"
-        />
-      </div>
-      <div className="flex space-x-4 mb-6">
-        {choices.map((choice) => (
-          <ChoiceButton
-            key={choice}
-            choice={choice}
-            onClick={() => handleChoice(choice)}
-            disabled={isAnimating}
+    <div className="h-screen flex items-center justify-center">
+      <div className=" flex flex-col items-center justify-center p-6 bg-gray-100 rounded-lg shadow-lg max-w-md w-full">
+        <h1>가위바위보 게임</h1>
+        <div className="flex justify-between w-full mb-8">
+          <Player
+            label="당신"
+            choice={userChoice}
+            score={score.user}
+            scoreColor="text-blue-600"
           />
-        ))}
-      </div>
-      <div className="text-center mt-4">
-        <button
-          className="mb-4 text-2xl cursor-pointer hover:font-bold"
-          onClick={handleReset}
-        >
-          Reset
-        </button>
-        <p className="text-gray-700 mb-1">아래 버튼을 눌러 선택하세요</p>
-        <div className="flex justify-center space-x-2 text-sm">
-          <span className="bg-gray-200 px-2 py-1 rounded">✊ = 바위</span>
-          <span className="bg-gray-200 px-2 py-1 rounded">✋ = 보</span>
-          <span className="bg-gray-200 px-2 py-1 rounded">✌️ = 가위</span>
+          <ResultDisplay result={result} />
+          <Player
+            label="컴퓨터"
+            choice={computerChice}
+            score={score.computer}
+            scoreColor="text-red-600"
+          />
+        </div>
+        <div className="flex space-x-4 mb-6">
+          {choices.map((choice) => (
+            <ChoiceButton
+              key={choice}
+              choice={choice}
+              onClick={() => handleChoice(choice)}
+              disabled={isAnimating}
+            />
+          ))}
+        </div>
+        <div className="text-center mt-4">
+          <button
+            className="mb-4 text-2xl cursor-pointer hover:font-bold"
+            onClick={handleReset}
+          >
+            Reset
+          </button>
+          <p className="text-gray-700 mb-1">아래 버튼을 눌러 선택하세요</p>
+          <div className="flex justify-center space-x-2 text-sm">
+            <span className="bg-gray-200 px-2 py-1 rounded">✊ = 바위</span>
+            <span className="bg-gray-200 px-2 py-1 rounded">✋ = 보</span>
+            <span className="bg-gray-200 px-2 py-1 rounded">✌️ = 가위</span>
+          </div>
         </div>
       </div>
     </div>
